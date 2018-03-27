32d259128d931305f266015e13fe3e5505deafa9
Aki <aakash_1201@yahoo.co.in>
2018-03-28 00:44:14 +0530

    Added ERPContext and Migration InitialModel

---
+++ ERPApplication/Migrations/201803271903508_InitialModel.cs
@@ -0,0 +1,30 @@
+namespace ERPApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerId = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        Lastname = c.String(),
+                        City = c.String(),
+                        Country = c.String(),
+                        Phone = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CustomerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
---
+++ ERPApplication/Models/ERPContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ERPApplication.Models
+{
+    public class ERPContext : DbContext
+    {
+        public ERPContext():base("name=DBCS")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
