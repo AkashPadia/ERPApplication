d78293e2ee4ed1caa348b6134e16527e2ec1ef18
Aki <aakash_1201@yahoo.co.in>
2018-04-01 23:28:34 +0530

    DisplayName DataAnotation added to product model class

--- ERPApplication/Models/Product.cs
+++ ERPApplication/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,18 @@ namespace ERPApplication.Models
     public class Product
     {
         public int ProductId { get; set; }
+
+        [DisplayName("Product Name")]
         public string ProductName { get; set; }
+
+        [DisplayName("Unit Price")]
         public float UnitPrice { get; set; }
+
         public string Package { get; set; }
+
         public bool IsDiscontinued { get; set; }
 
+        [DisplayName("Supplier Name")]
         public int SupplierId { get; set; }
 
         public Supplier Suppliers { get; set; }
